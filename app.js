051f4518a2eff61940a6ebb80e18a5360e936554
zakk <muhammadzakaria616@gmail.com>
2022-07-31 22:50:34 +0500

    created server

---
+++ app.js
@@ -0,0 +1,32 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", {
+  useNewUrlParser: true,
+});
+
+const articleSchema = new mongoose.Schema({
+    title: String,
+    content: String
+});
+
+const Article = mongoose.model("Article", articleSchema);
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
